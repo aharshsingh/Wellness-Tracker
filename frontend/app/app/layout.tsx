8b7b90240d6619707418c862017d69b46eb11855
aharshsingh <aharsh@abcdeventures.com>
2025-08-25 12:08:44 +0530

    feat: layout

---
+++ frontend/app/app/layout.tsx
@@ -0,0 +1,18 @@
+// components/Layout.tsx
+import {Navbar} from "@/components/ui/navbar";
+import {Footer} from "@/components/ui/footer";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      
+      {/* Main content area */}
+      <main className="flex-grow container mx-auto px-6 py-8">
+        {children}
+      </main>
+      
+      <Footer />
+    </div>
+  );
+}
